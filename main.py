d433aa03848df20870c10878777c9e2cf9f726fe
Panashe816 <pganyani93@gmail.com>
2025-12-03 19:23:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,30 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine, text
+import os
 
 app = FastAPI()
 
-DATABASE_URL = "postgresql://universalnews_user:V5niNC00UBFDXwyOvFJhCLjiYHFXZi8b@dpg-d4a28v7diees73cr0bm0-a.virginia-postgres.render.com/universalnews"
-
+DATABASE_URL = os.getenv("DATABASE_URL") or "postgresql://universalnews_user:V5niNC00UBFDXwyOvFJhCLjiYHFXZi8b@dpg-d4a28v7diees73cr0bm0-a.virginia-postgres.render.com/universalnews"
 engine = create_engine(DATABASE_URL)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 @app.get("/")
-def home():
+def root():
     return {"message": "Viral News API is running"}
 
 @app.get("/news")
 def get_news():
     query = text("""
         SELECT 
-            id, 
-            title, 
-            headline, 
-            article, 
-            category, 
-            created_at
+            id,
+            title,
+            content,
+            image_url,
+            category,
+            created_at,
+            source_url
         FROM articles
         ORDER BY created_at DESC
-        LIMIT 50;
+        LIMIT 50
     """)
     
     with engine.connect() as conn:
@@ -42,10 +35,11 @@ def get_news():
         results.append({
             "id": r.id,
             "title": r.title,
-            "headline": r.headline,
-            "article": r.article,
+            "content": r.content,
+            "image_url": r.image_url,
             "category": r.category,
             "created_at": str(r.created_at),
+            "source_url": r.source_url
         })
     
     return results
